03a8ef76e8790397ebd2a5fddf627b74b409ed67
kyle suhan <hourpathmeditations@gmail.com>
2022-03-03 15:00:40 -0500

    landing page update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,57 +1,81 @@
-import { Container, Col, Row } from "react-bootstrap";
 import scarabLogo from "./scarab-logo.png";
+import {
+  Container,
+  Grid
+} from "@mui/material";
 
 const styles = {
-  container: {
+  logoContainer: {
     height: "900px",
     paddingBottom: "0px",
+    marginTop: "80px"
   },
   logoBoxPadding: {
     paddingTop: "50px",
   },
+  logoBackground: {
+    height: '100%',
+    width: "auto"
+  },
+  imageContainer: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  scarabLogo: {
+    width:  "250px",
+    height:  "250px",
+    borderRadius: "50%",
+    boxShadow: '0px 10px 12px black'
+  }
 };
 
 export default function Landing() {
   return (
-    <div style={{ display: "flex" }}>
-      <Container style={styles.container}>
-        <div className="main-logo-box" style={styles.logoBoxPadding}>
-          <Row>
-            <Col className="vertical-center logo-text">Sacred</Col>
-            <Col>
-              <div className="logobox">
-                <div className="logoHelper">
-                  <img
-                    src={scarabLogo}
-                    className="scarab-logo"
-                    alt="scarab-logo"
-                  />
-                </div>
-              </div>
-            </Col>
-            <Col className="vertical-center logo-text">Scarabs</Col>
-          </Row>
-        </div>
-        <Row>
-          <Col className="headingText">
-            <p>
-              The mission of the Sacred Scarabs NFT community and decentralized
-              application is to provide crowd-funding to members who are
-              actively working towards planetary healing.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col className="subHeadingText">
-            <p>
-              {" "}
-              View available NFTs available on{" "}
-              <a href="https://opensea.io/collection/sacred-scarabs">
-                Opensea.io
-              </a>
-            </p>
-          </Col>
-        </Row>
+    <div>
+      <Container style={styles.logoContainer}>
+        <Grid container>
+          <Grid item xs={12} md={4} justifyContent="center" alignItems="center">
+            <p className="logo-text">Sacred</p>
+          </Grid>
+          <Grid item xs={12} md={4} height="250px">
+            <div style={styles.logoBackground}>
+              <div style={styles.imageContainer}>    
+                <img
+                  style={styles.scarabLogo}
+                  src={scarabLogo}
+                  alt="scarab-logo"
+                /> 
+              </div>      
+            </div>
+          </Grid>
+          <Grid item xs={12} md={4} justifyContent="center" alignItems="center">
+            <p className="logo-text">Scarabs</p>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <div className="headingText">
+              <p>
+                The mission of the Sacred Scarabs NFT community and decentralized
+                application is to provide crowd-funding to members who are
+                actively working towards planetary healing.
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <div className="subHeadingText">
+              <p>
+                {" "}
+                View available NFTs available on{" "}
+                <a href="https://opensea.io/collection/sacred-scarabs">
+                  Opensea.io
+                </a>
+              </p>
+            </div>          
+          </Grid>      
+        </Grid>
       </Container>
     </div>
   );
