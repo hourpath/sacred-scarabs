157f65d1a407f8f79efb123c82b3a23958d9af53
erik123567 <erik123567@gmail.com>
2022-03-08 06:48:02 -0500

    Update Profile.jsx
    Toggle for address form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,45 +2,58 @@ import { Container, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useMoralis } from "react-moralis";
 import { useState, useEffect } from "react";
-  import { Moralis } from 'moralis';
+import { Moralis } from "moralis";
+import Switch from '@mui/material/Switch';
 
 export default function Profile() {
   const { user, setUserData } = useMoralis();
   const [email, setEmail] = useState("");
-  const [avatarFile, setAvatarFile] = useState('');
+  const [avatarFile, setAvatarFile] = useState("");
   const [username, setUserName] = useState("");
+  const [showAddress, setShowAddress] = useState(false);
 
-
-  useEffect(()=>{
-    if(user){
-      setUserName(user.attributes.username)
-      const userEmail = user.get('email');
-      if(userEmail){
+  useEffect(() => {
+  
+    if (user) {
+      setUserName(user.attributes.username);
+      const userEmail = user.get("email");
+      if (userEmail) {
         setEmail(userEmail);
       }
       const userAvatar = user?.attributes?.avatar?._url;
-          if(userAvatar) {
-            setAvatarFile(userAvatar)
+      if (userAvatar) {
+        setAvatarFile(userAvatar);
+      }
+      console.log(user?.attributes)
+      const showAddressIn = user?.attributes.showAddress;
+      if(showAddressIn){
+        console.log(showAddressIn);
+        setShowAddress(showAddressIn);
       }
     }
-  },[user])
+  }, [user]);
 
   if (!user) {
     //REDIRECT TO HOME?
     return <h1>Please login</h1>;
   }
 
+  const handleToggle = () => {
+    setShowAddress(!showAddress)
+  }
+
   const handleSave = () => {
     const profilePic = new Moralis.File(avatarFile.name, avatarFile);
+
     setUserData({
       email: email,
       avatarFile: profilePic,
-      username: username
+      username: username,
+      showAddress: showAddress
     });
   };
 
   return (
-    
     <div>
       <Container>
         <Grid container>
@@ -50,33 +63,65 @@ export default function Profile() {
                 Sacred Scarabs Profile Dashboard (COMING SOON!)
               </Typography>
             </div>
-            {user && (<div>
-              <h1>Welcome user {username}</h1>
+            {user && (
               <div>
-              <label htmlFor="fileAvatar">Select Avatar</label>
-              <input type="file" id="fileAvatar" defaultValue="" onChange={(e) => setAvatarFile(e.target.files[0])}></input>
-              <img src= {user?.attributes.avatarFile._url} style={{width:'200px', height:'200px'}}></img>
+                <h1>Welcome user {username}</h1>
+                <div>
+                  <label htmlFor="fileAvatar">Select Avatar</label>
+                  <input
+                    type="file"
+                    id="fileAvatar"
+                    defaultValue=""
+                    onChange={(e) => setAvatarFile(e.target.files[0])}
+                  ></input>
+                  <img
+                    src={user?.attributes.avatarFile._url}
+                    style={{ width: "200px", height: "200px" }}
+                  ></img>
+                </div>
+                <label htmlFor="username">
+                  UserName:
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUserName(e.currentTarget.value)}
+                  ></input>
+                </label>
+
+                <label htmlFor="email">
+                  Email:
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                  ></input>
+                </label>
+                <button onClick={handleSave}>Save</button>
+
+                <Switch
+                  checked={showAddress}
+                  onChange={handleToggle}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                    />
               </div>
-              <label htmlFor='username'>
-                UserName:
-                <input type='text' id='username' value={username} onChange={(e) => setUserName(e.currentTarget.value)}>
-                </input>
-              </label>
-              
-              <label htmlFor="email">Email:
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.currentTarget.value)}
-              ></input>
-              </label>
-              <button onClick={handleSave}>Save</button>
-            </div>)}
-            
+            )}
+            {user  && showAddress && (
+              <div>
+                {/* <form>
+                  <input>ABC ETC</input>
+                
+                </form> */}
+                Show address form
+              </div>
+            )}
           </Grid>
         </Grid>
       </Container>
     </div>
   );
+
+
 }
+
