c0f2fa8cf9f1fa813ac2f19a73c60da2488df2eb
erik123567 <erik123567@gmail.com>
2022-03-08 03:27:28 -0500

    username, profile pic and email all work functionally

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,33 +1,46 @@
 import { Container, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useMoralis } from "react-moralis";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+  import { Moralis } from 'moralis';
 
 export default function Profile() {
-  const {  user, setUserData } = useMoralis();
-  console.log(user);
-  const [email, setEmail] = useState(user.attributes.email);
+  const { user, setUserData } = useMoralis();
+  const [email, setEmail] = useState("");
+  const [avatarFile, setAvatarFile] = useState('');
+  const [username, setUserName] = useState("");
 
 
-  if(!user){
+  useEffect(()=>{
+    if(user){
+      setUserName(user.attributes.username)
+      const userEmail = user.get('email');
+      if(userEmail){
+        setEmail(userEmail);
+      }
+      const userAvatar = user?.attributes?.avatar?._url;
+          if(userAvatar) {
+            setAvatarFile(userAvatar)
+      }
+    }
+  },[user])
+
+  if (!user) {
     //REDIRECT TO HOME?
-    return (<h1>Please login</h1>)
+    return <h1>Please login</h1>;
   }
 
- const handleSave = () =>{
-   console.log('saved' + email)
+  const handleSave = () => {
+    const profilePic = new Moralis.File(avatarFile.name, avatarFile);
     setUserData({
       email: email,
-    }
-      
-    )
-  }
+      avatarFile: profilePic,
+      username: username
+    });
+  };
 
-  
-
-  
   return (
+    
     <div>
       <Container>
         <Grid container>
@@ -37,18 +50,30 @@ export default function Profile() {
                 Sacred Scarabs Profile Dashboard (COMING SOON!)
               </Typography>
             </div>
-            <div>
-            <h1>Welcome user {user.id}</h1>
-
-              <img src="" id='imgAvatar' alt=''></img>
-              <label htmlFor='fileAvatar'>Select Avatar</label>
-              <input type='file' id='fileAvatar'></input>
-              <label htmlFor='email'>Email:</label>
-              <input type='email' id='email' value={email} onChange={(e) => setEmail(e.currentTarget.value)}></input>
-          <button onClick={handleSave}>Save</button>
-
-
-            </div>
+            {user && (<div>
+              <h1>Welcome user {username}</h1>
+              <div>
+              <label htmlFor="fileAvatar">Select Avatar</label>
+              <input type="file" id="fileAvatar" defaultValue="" onChange={(e) => setAvatarFile(e.target.files[0])}></input>
+              <img src= {user?.attributes.avatarFile._url} style={{width:'200px', height:'200px'}}></img>
+              </div>
+              <label htmlFor='username'>
+                UserName:
+                <input type='text' id='username' value={username} onChange={(e) => setUserName(e.currentTarget.value)}>
+                </input>
+              </label>
+              
+              <label htmlFor="email">Email:
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              ></input>
+              </label>
+              <button onClick={handleSave}>Save</button>
+            </div>)}
+            
           </Grid>
         </Grid>
       </Container>
