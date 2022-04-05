26c09d2a49828c3bbfe8a03cf65e67800aca5f2d
erik123567 <erik123567@gmail.com>
2022-04-05 18:48:14 -0400

    render setup

--- src/components/HealingNetwork.jsx
+++ src/components/HealingNetwork.jsx
@@ -8,20 +8,23 @@ import Button from "@mui/material/Button";
 import { useState, useEffect } from "react";
 import { Moralis } from "moralis";
 
-
 export default function HealingNetwork() {
   const [users, setUsers] = useState([]);
-
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const results = await Moralis.Cloud.run("getPractitioners", {});
-      console.log(results)
+      console.log(results);
       setUsers(results);
       for (let i = 0; i < results.length; i++) {
+        //const object = results[i].attributes;
         const object = results[i];
-        console.log(object.id + " - " + object.get("name") + object.get('username'));
+        console.log(
+         object
+        );
       }
+      setLoaded(true);
     };
 
     fetchData();
@@ -38,10 +41,42 @@ export default function HealingNetwork() {
 
   // getUsers();
 
+  const renderCards = () =>{
+    console.log(users)
+      users.map((practitioner)=>{
+        return (
+          <Grid item xs={6} md={4} lg={3} style={{ border: "5px red" }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {practitioner.username}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Becky G is a tarot reader with over 25 years of experience she
+                is a friendly person...
+              </Typography>
+              <Typography variant="body3" color="text.secondary">
+                Remote
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Profile</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        );
+      })
+  }
+
   return (
     <div>
       <Container>
-        {users.length}
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <div className="headingText">
@@ -183,6 +218,7 @@ export default function HealingNetwork() {
               </CardActions>
             </Card>
           </Grid>
+          {loaded ? renderCards(): "NOT LOADED"}
         </Grid>
       </Container>
     </div>
