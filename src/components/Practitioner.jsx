e1d68a5f5521f38a919856ea3dfa7630a2d6126b
erik123567 <erik123567@gmail.com>
2022-04-11 17:25:57 -0400

    style

--- src/components/Practitioner.jsx
+++ src/components/Practitioner.jsx
@@ -10,7 +10,7 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import BasicTabs from "./BasicTabs";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 
 export default function HealingNetwork() {
   let { username } = useParams();
@@ -53,51 +53,54 @@ export default function HealingNetwork() {
     <div>
       {loaded && user ? (
         <div>
-          <Grid item xs={12} style={{height: '40vh'}}>
-              <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Grid item xs={4} container justifyContent='center'>
-                  <Avatar src={user.avatarFile._url} sx={{width:'25vh', height: '25vh'}}></Avatar>
-                </Grid>
+          <Grid item xs={12} style={{ height: "40vh" }}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item xs={4} container justifyContent="center">
+                <Avatar
+                  src={user.avatarFile._url}
+                  sx={{ width: "25vh", height: "25vh" }}
+                ></Avatar>
               </Grid>
-              <Grid
-                container
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Grid item xs={12}>
-                  <h1>{user.username}</h1>
-                </Grid>
-                <Grid item xs={12}>
-                  {user.about}
-                </Grid>
-                <Grid item xs={12}>
-                  {" "}
-                  <a
-                    href={user.websiteURL}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {user.websiteURL}
-                  </a>
-                </Grid>
-                <Grid item xs={12}>
-                  <FacebookIcon />
-                  <YouTubeIcon />
-                  <InstagramIcon />
-                  <TwitterIcon />
-                </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item xs={12}>
+                <h1>{user.username}</h1>
               </Grid>
+              <Grid item xs={12}>
+                {user.about}
+              </Grid>
+              <Grid item xs={12}>
+                {" "}
+                <a
+                  href={user.websiteURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {user.websiteURL}
+                </a>
+              </Grid>
+              <Grid item xs={12}>
+                <FacebookIcon />
+                <YouTubeIcon />
+                <InstagramIcon />
+                <TwitterIcon />
+              </Grid>
+            </Grid>
           </Grid>
 
           <Grid>{<BasicTabs user={user} />}</Grid>
 
-          <Grid item xs={12} direction="row" style={{marginBottom: '5px'}}>
+          <Grid item xs={12} direction="row" style={{ marginBottom: "5px" }}>
             <Card>
               <Grid container direction="row">
                 <Grid item xs={2}>
@@ -106,7 +109,7 @@ export default function HealingNetwork() {
                     height="200"
                     image={user.avatarFile._url}
                     alt={user.avatarFile._name}
-                    style={{'padding': '5px', 'paddingRight' : '10px'}}
+                    style={{ padding: "5px", paddingRight: "10px" }}
                   />
                 </Grid>
                 <Grid item xs={10}>
@@ -123,7 +126,7 @@ export default function HealingNetwork() {
               </Grid>
             </Card>
           </Grid>
-          <Grid item xs={12} direction="row" style={{marginBottom: '5px'}}>
+          <Grid item xs={12} direction="row" style={{ marginBottom: "5px" }}>
             <Card>
               <Grid container direction="row">
                 <Grid item xs={2}>
@@ -132,7 +135,7 @@ export default function HealingNetwork() {
                     height="200"
                     image={user.avatarFile._url}
                     alt={user.avatarFile._name}
-                    style={{'padding': '5px', 'paddingRight' : '10px'}}
+                    style={{ padding: "5px", paddingRight: "10px" }}
                   />
                 </Grid>
                 <Grid item xs={10}>
@@ -149,7 +152,7 @@ export default function HealingNetwork() {
               </Grid>
             </Card>
           </Grid>
-          <Grid item xs={12} direction="row" style={{marginBottom: '20px'}}>
+          <Grid item xs={12} direction="row" style={{ marginBottom: "20px" }}>
             <Card>
               <Grid container direction="row">
                 <Grid item xs={2}>
@@ -158,7 +161,7 @@ export default function HealingNetwork() {
                     height="200"
                     image={user.avatarFile._url}
                     alt={user.avatarFile._name}
-                    style={{'padding': '5px', 'paddingRight' : '10px'}}
+                    style={{ padding: "5px", paddingRight: "10px" }}
                   />
                 </Grid>
                 <Grid item xs={10}>
